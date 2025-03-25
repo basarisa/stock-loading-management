2dc13bdd974264be8d35a42873aaa2decb34e8a5
Arisa Teandum <basarisa.1302@gmail.com>
2025-03-25 18:19:10 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Stock Loading Management
+
+This project is a simple stock loading management system.
+
+## Features
+
+-
+
+## Installation
+
+1. Clone the repository:
+
+```sh
+git clone https://github.com/yourusername/stock-loading-management.git
+```
+
+2. Navigate to the project directory:
+
+```sh
+cd stock-loading-management
+```
+
+3. Install dependencies:
+
+```sh
+npm install
+```
+
+## Usage
+
+1. Set enviroments
+
+2. Start the application:
+
+```sh
+npm start
+```
+
+3. Open your browser and navigate to ...
---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config(); // โหลดค่าจากไฟล์ .env
+
+const express = require("express");
+const cors = require("cors");
+// const supabase = require("./supabaseClient");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route ทดสอบ API
+app.get("/", (req, res) => {
+  console.log("Got a call here /");
+  res.send("Hello from Express.js Backend!");
+});
+
+// เริ่มต้น Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
